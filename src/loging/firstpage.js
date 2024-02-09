51b10354ba88dcaae3438a89d5658c86645efc65
Mahak <rathoremahak028@gmail.com>
2024-02-09 19:17:41 +0530

    class change

--- src/loging/firstpage.js
+++ src/loging/firstpage.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Firstpage = () => {
   return (
     <div>
-      <div class="main-div">
+      <div class="main-div-for-first">
         <Link to="/onboard">
         <img src={icon} alt='icon' />
         </Link>
